0215ce21ce8cf44d7b080b7aa04b7248770a11a2
Ariel_Naiman <a0527633600@gmail.com>
2026-02-09 17:35:40 +0200

    commit

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ let data;
 const local = localStorage.getItem("news")
 
 // if (!local) {
-await fetch("https://newsapi.org/v2/everything?q=news&language=ar&apiKey=be2f6dc8cd7a4e9086a516f0a4f06aff")
+    // await fetch("https://newsapi.org/v2/everything?q=news&language=he&apiKey=be2f6dc8cd7a4e9086a516f0a4f06aff")
+    // await fetch("https://newsapi.org/v2/everything?q=news&language=he&apiKey=be2f6dc8cd7a4e9086a516f0a4f06aff")
+    await fetch("https://newsapi.org/v2/top-headlines?country=us&apiKey=e6d0cc2435c44129a5f0804ad952d9e1")
     .then(val => val.json())
     .then(val => data = val.articles)
 // localStorage.setItem("news", JSON.stringify(data))
